399ab3a13d6f15e66b3ff52c8965317c8397c9d5
jeff.hykin <jeff.hykin@gmail.com>
2021-05-27 12:28:30 -0500

    add basic tools

---
+++ main/tools/basics.py
@@ -0,0 +1,40 @@
+def is_iterable(thing):
+    # https://stackoverflow.com/questions/1952464/in-python-how-do-i-determine-if-an-object-is-iterable
+    try:
+        iter(thing)
+    except TypeError:
+        return False
+    else:
+        return True
+
+def flatten(value):
+    flattener = lambda *m: (i for n in m for i in (flattener(*n) if is_iterable(n) else (n,)))
+    return list(flattener(value))
+
+
+import collections.abc
+def merge(old_value, new_value):
+    # if not dict, see if it is iterable
+    if not isinstance(new_value, collections.abc.Mapping):
+        if is_iterable(new_value):
+            new_value = { index: value for index, value in enumerate(new_value) }
+    
+    # if still not a dict, then just return the current value
+    if not isinstance(new_value, collections.abc.Mapping):
+        return new_value
+    # otherwise get recursive
+    else:
+        # if not dict, see if it is iterable
+        if not isinstance(old_value, collections.abc.Mapping):
+            if is_iterable(old_value):
+                old_value = { index: value for index, value in enumerate(old_value) }
+        # if still not a dict
+        if not isinstance(old_value, collections.abc.Mapping):
+            # force it to be one
+            old_value = {}
+        
+        # override each key recursively
+        for key, updated_value in new_value.items():
+            old_value[key] = merge(old_value.get(key, {}), updated_value)
+        
+        return old_value
