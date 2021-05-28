cd781c850ba800834412381cb6d8ae9725e2a483
jeff.hykin <jeff.hykin@gmail.com>
2021-05-27 19:06:59 -0500

    add package, fix SSL more

--- settings/setup_automatically/steps/01900_setup_python_venv.sh
+++ settings/setup_automatically/steps/01900_setup_python_venv.sh
@@ -8,3 +8,6 @@ then
     echo "creating virtual env for python"
     python -m venv "$VIRTUAL_ENV" && echo "virtual env created"
 fi
+
+# fix SSL issues
+export SSL_CERT_FILE="$(python -c 'import ssl; print(ssl.get_default_verify_paths().openssl_cafile)')"  
