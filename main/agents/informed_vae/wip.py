04e5471b9c6f8c973ae7e34e328db4ba33d48e88
jeff.hykin <jeff.hykin@gmail.com>
2021-07-01 13:42:49 -0500

    custom gradient decent

--- main/agents/informed_vae/wip.py
+++ main/agents/informed_vae/wip.py
@@ -1,15 +1,181 @@
 from agents.informed_vae.main import ImageEncoder, ImageModelSequential
 
 import torch
+import torchvision
 from tools.file_system_tools import FS
 from tools.dataset_tools import Mnist
 from tools.pytorch_tools import read_image, to_tensor
 from torchvision import transforms
 import torch.nn.functional as F
 
+# 
+# 
+# optimizer
+# 
+# 
 import torch
-import torchvision
+import torch.nn.functional as F
+from torch.optim.optimizer import Optimizer, required
 
+def sgd(
+    params,
+    d_p_list,
+    momentum_buffer_list,
+    *,
+    weight_decay: float,
+    momentum: float,
+    lr: float,
+    dampening: float,
+    nesterov: bool
+):
+    r"""
+    Functional API that performs SGD algorithm computation.
+    See :class:`~torch.optim.SGD` for details.
+    """
+    for index, param in enumerate(params):
+
+        d_p = d_p_list[index]
+        if weight_decay != 0:
+            d_p = d_p.add(param, alpha=weight_decay)
+
+        if momentum != 0:
+            buf = momentum_buffer_list[index]
+
+            if buf is None:
+                buf = torch.clone(d_p).detach()
+                momentum_buffer_list[index] = buf
+            else:
+                buf.mul_(momentum).add_(d_p, alpha=1 - dampening)
+
+            if nesterov:
+                d_p = d_p.add(buf, alpha=momentum)
+            else:
+                d_p = buf
+
+        param.add_(d_p, alpha=-lr)
+
+
+class SGD(Optimizer):
+    r"""Implements stochastic gradient descent (optionally with momentum).
+
+    Nesterov momentum is based on the formula from
+    `On the importance of initialization and momentum in deep learning`__.
+
+    Args:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float): learning rate
+        momentum (float, optional): momentum factor (default: 0)
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+        dampening (float, optional): dampening for momentum (default: 0)
+        nesterov (bool, optional): enables Nesterov momentum (default: False)
+
+    Example:
+        >>> optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+        >>> optimizer.zero_grad()
+        >>> loss_fn(model(input), target).backward()
+        >>> optimizer.step()
+
+    __ http://www.cs.toronto.edu/%7Ehinton/absps/momentum.pdf
+
+    .. note::
+        The implementation of SGD with Momentum/Nesterov subtly differs from
+        Sutskever et. al. and implementations in some other frameworks.
+
+        Considering the specific case of Momentum, the update can be written as
+
+        .. math::
+            \begin{aligned}
+                v_{t+1} & = \mu * v_{t} + g_{t+1}, \\
+                p_{t+1} & = p_{t} - \text{lr} * v_{t+1},
+            \end{aligned}
+
+        where :math:`p`, :math:`g`, :math:`v` and :math:`\mu` denote the
+        parameters, gradient, velocity, and momentum respectively.
+
+        This is in contrast to Sutskever et. al. and
+        other frameworks which employ an update of the form
+
+        .. math::
+            \begin{aligned}
+                v_{t+1} & = \mu * v_{t} + \text{lr} * g_{t+1}, \\
+                p_{t+1} & = p_{t} - v_{t+1}.
+            \end{aligned}
+
+        The Nesterov version is analogously modified.
+    """
+
+    def __init__(self, params, lr=required, momentum=0, dampening=0,
+                 weight_decay=0, nesterov=False):
+        if lr is not required and lr < 0.0:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if momentum < 0.0:
+            raise ValueError("Invalid momentum value: {}".format(momentum))
+        if weight_decay < 0.0:
+            raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
+
+        defaults = dict(lr=lr, momentum=momentum, dampening=dampening,
+                        weight_decay=weight_decay, nesterov=nesterov)
+        if nesterov and (momentum <= 0 or dampening != 0):
+            raise ValueError("Nesterov momentum requires a momentum and zero dampening")
+        super(SGD, self).__init__(params, defaults)
+
+    def __setstate__(self, state):
+        super(SGD, self).__setstate__(state)
+        for group in self.param_groups:
+            group.setdefault('nesterov', False)
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        """Performs a single optimization step.
+
+        Args:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            params_with_grad = []
+            d_p_list = []
+            momentum_buffer_list = []
+            weight_decay = group['weight_decay']
+            momentum = group['momentum']
+            dampening = group['dampening']
+            nesterov = group['nesterov']
+            lr = group['lr']
+
+            for p in group['params']:
+                if p.grad is not None:
+                    params_with_grad.append(p)
+                    d_p_list.append(p.grad)
+
+                    state = self.state[p]
+                    if 'momentum_buffer' not in state:
+                        momentum_buffer_list.append(None)
+                    else:
+                        momentum_buffer_list.append(state['momentum_buffer'])
+            
+            sgd(
+                params_with_grad,
+                d_p_list,
+                momentum_buffer_list,
+                weight_decay=weight_decay,
+                momentum=momentum,
+                lr=lr,
+                dampening=dampening,
+                nesterov=nesterov
+            )
+
+            # update momentum_buffers in state
+            for p, momentum_buffer in zip(params_with_grad, momentum_buffer_list):
+                state = self.state[p]
+                state['momentum_buffer'] = momentum_buffer
+
+        return loss
 
 #
 # network
@@ -42,85 +208,20 @@ class ImageEncoder(ImageModelSequential):
             self.layers.add_module("fc2", nn.Linear(self.size_of_last_layer, 10))
             self.layers.add_module("fc2_activation", nn.LogSoftmax(dim=-1))
         
-        self.optimizer = optim.SGD(self.parameters(), lr=self.learning_rate, momentum=self.momentum)
+        def NLLLoss(batch_of_actual_outputs, batch_of_ideal_outputs):
+            output = batch_of_actual_outputs[range(len(batch_of_ideal_outputs)), batch_of_ideal_outputs]
+            return -output.sum()/len(output)
+        
+        self.loss_function = NLLLoss
+        self.optimizer = SGD(self.parameters(), lr=self.learning_rate, momentum=self.momentum)
     
     def update_weights(self, batch_of_inputs, batch_of_ideal_outputs, epoch_index, batch_index):
         self.optimizer.zero_grad()
-        actual_output = self.forward(batch_of_inputs)
-        def NLLLoss(logs, targets):
-            out = logs[range(len(targets)), targets]
-            return -out.sum()/len(out)
-        
-        loss = NLLLoss(actual_output, batch_of_ideal_outputs)
-        
+        batch_of_actual_outputs = self.forward(batch_of_inputs)
+        loss = self.loss_function(batch_of_actual_outputs, batch_of_ideal_outputs)
         loss.backward()
         self.optimizer.step()
         return loss
-            
-    def fit(self, input_output_pairs=None, dataset=None, loader=None, number_of_epochs=3, batch_size=64, shuffle=True):
-        """
-        Examples:
-            model.fit(
-                dataset=torchvision.datasets.MNIST(<mnist args>),
-                epochs=4,
-                batch_size=64,
-            )
-            
-            model.fit(
-                loader=torch.utils.data.DataLoader(<dataloader args>),
-                epochs=4,
-            )
-        """
-        
-        if input_output_pairs is not None:
-            # creates batches
-            def bundle(iterable, bundle_size):
-                next_bundle = []
-                for each in iterable:
-                    next_bundle.append(each)
-                    if len(next_bundle) == bundle_size:
-                        yield tuple(next_bundle)
-                        next_bundle = []
-                # return any half-made bundles
-                if len(next_bundle) > 0:
-                    yield tuple(next_bundle)
-            # unpair, batch, then re-pair the inputs and outputs
-            input_generator        = (each for each, _ in input_output_pairs)
-            ideal_output_generator = (each for _   , each in input_output_pairs)
-            seperated_batches = zip(bundle(input_generator, batch_size), bundle(ideal_output_generator, batch_size))
-            loader = ((to_tensor(each_input_batch), to_tensor(each_output_batch)) for each_input_batch, each_output_batch in seperated_batches)
-            # NOTE: shuffling isn't possible when there is no length (and generators don't have lengths). So maybe think of an alternative
-        else:
-            # convert the dataset into a loader (assumming loader was not given)
-            if isinstance(dataset, torch.utils.data.Dataset):
-                loader = torch.utils.data.DataLoader(
-                    dataset,
-                    batch_size=batch_size,
-                    shuffle=shuffle,
-                )
-        
-        train_losses = []
-        for epoch_index in range(number_of_epochs):
-            self.train()
-            for batch_index, (batch_of_inputs, batch_of_ideal_outputs) in enumerate(loader):
-                loss = self.update_weights(batch_of_inputs, batch_of_ideal_outputs, epoch_index, batch_index)
-                if batch_index % self.log_interval == 0:
-                    self.print(
-                        "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
-                            epoch_index+1,
-                            batch_index * len(batch_of_inputs),
-                            len(loader.dataset),
-                            100.0 * batch_index / len(loader),
-                            loss.item(),
-                        )
-                    )
-                    train_losses.append(loss.item())
-                    # TODO: add/allow checkpoints
-                    # import os
-                    # os.makedirs(f"{temp_folder_path}/results/", exist_ok=True)
-                    # torch.save(self.state_dict(), f"{temp_folder_path}/results/model.pth")
-                    # torch.save(self.optimizer.state_dict(), f"{temp_folder_path}/results/optimizer.pth")
-        return train_losses
             
     def test(self, test_loader):
         test_losses = []
