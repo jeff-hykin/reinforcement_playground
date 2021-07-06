3eabb16b7a3f0c4d5f05cab4ad6fbbd5b8a5ada7
Jeff Hykin <jeff.hykin@gmail.com>
2021-07-06 11:46:24 -0500

    make output easier to run

--- main/agents/informed_vae/mnist_test.py
+++ main/agents/informed_vae/mnist_test.py
@@ -1,27 +1,90 @@
-from agents.informed_vae.main import ImageEncoder
+from agents.informed_vae.wip import * 
+b = ImageAutoEnccoder()
 
-import torch
-from tools.file_system_tools import FS
-from tools.dataset_tools import Mnist
-from tools.pytorch_tools import read_image, to_tensor
-from torchvision import transforms
-import torch.nn.functional as F
+class AutoMnist(torchvision.datasets.MNIST):
+    def __init__(self, *args, **kwargs):
+        super(AutoMnist, self).__init__(*args, **kwargs)
+    
+    def __getitem__(self, index):
+        an_input, corrisponding_output = super(AutoMnist, self).__getitem__(index)
+        return an_input, an_input
 
-# preprocess data, and make it an iterator of input-output pairs
-transformed_mnist_pairs = Mnist(
-    # convert image-tensor to a float tensor then normalize
-    transform_input=lambda each_image: transforms.Normalize((0.5,), (0.5,))(each_image.type(torch.float)),
-    # convert number to one-hot encoding
-    transform_output=lambda each_number: F.one_hot(to_tensor(each_number), num_classes=Mnist.number_of_classes).type(torch.float),
+from tools.basics import temp_folder
+train_loader = torch.utils.data.DataLoader(
+    AutoMnist(
+        f"{temp_folder}/files/",
+        train=True,
+        download=True,
+        transform=torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        ),
+    ),
+    batch_size=64,
+    shuffle=True,
 )
 
-dummy_encoder = ImageEncoder()
-encoded_output = dummy_encoder.forward(transformed_mnist_pairs[0][0])
-print((encoded_output * 1000).type(torch.int))
-# TODO: why would this value not be close to uniform?
+test_loader = torch.utils.data.DataLoader(
+    AutoMnist(
+        f"{temp_folder}/files/",
+        train=False,
+        download=True,
+        transform=torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        ),
+    ),
+    batch_size=1000,
+    shuffle=True,
+)
 
-print('training on Mnist')
-dummy_encoder.fit(transformed_mnist_pairs)
-for each in [ 31, 40 ]:
-    print('actual: ', transformed_mnist_pairs[each][1])
-    print('predicted: ', (dummy_encoder.forward(transformed_mnist_pairs[each][0]) * 1000).type(torch.int))
+train_dataset = AutoMnist(
+        f"{temp_folder}/files/",
+        train=True,
+        download=True,
+        transform=torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        ),
+    )
+test_dataset = AutoMnist(
+        f"{temp_folder}/files/",
+        train=False,
+        download=True,
+        transform=torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        ),
+    )
+
+b.fit(loader=train_loader, number_of_epochs=3)
+
+latent_spaces_for_training  = to_tensor( torch.from_numpy(b.encoder(train_dataset[index][0]).cpu().detach().numpy()) for index in range(len(train_dataset)) if index < 10)
+latent_spaces_for_testing   = to_tensor( torch.from_numpy(b.encoder(test_dataset[index][0]).cpu().detach().numpy()) for index in range(len(test_dataset)) if index < 1)
+
+import shap
+
+model = nn.Sequential(b.decoder, nn.Flatten())
+explainer = shap.DeepExplainer(model, latent_spaces_for_training)
+shap_values = explainer.shap_values(latent_spaces_for_testing)
+
+
+import numpy
+import functools
+# sum these up elementwise
+summed = numpy.squeeze(functools.reduce(
+    lambda each_new, existing: numpy.add(each_new, existing),
+    # take the absolute value because we just want impactful values, not just neg/pos correlated ones
+    numpy.abs(shap_values),
+    numpy.zeros_like(shap_values[0]),
+))
+
+print('summed = ', summed)
