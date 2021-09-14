67cb4357c7d0e55a9291ef8eac4f548909ef2b96
jeff.hykin <jeff.hykin@gmail.com>
2021-09-14 12:14:47 -0500

    add carla client

--- settings/during_start/021_001_get_around_broken_poetry.sh
+++ settings/during_start/021_001_get_around_broken_poetry.sh
@@ -1,1 +1,8 @@
 python -m pip --disable-pip-version-check install "optuna>=2.9.1"
+
+if [ "$OSTYPE" = "linux-gnu" ] 
+then
+    python -m pip --disable-pip-version-check install "carla-client-unofficial>=0.9.11"
+else
+    python -m pip --disable-pip-version-check install "carla>=0.9.12"
+fi
