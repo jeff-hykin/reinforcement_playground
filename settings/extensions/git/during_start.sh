adb419c09dc00a8e0ef0847e35692603a1985f02
jeff.hykin <jeff.hykin@gmail.com>
2021-09-14 08:09:37 -0500

    add the git subrepo extension

--- settings/extensions/git/during_start.sh
+++ settings/extensions/git/during_start.sh
@@ -1,7 +1,7 @@
 # run the git subrepo setup
-if [ -d "$FORNIX_FOLDER/settings/extensions/git/git_subrepo/" ]
+if [ -d "$FORNIX_FOLDER/settings/extensions/git/subrepo/" ]
 then
-    export GIT_SUBREPO_ROOT="$FORNIX_FOLDER/settings/extensions/git/git_subrepo/"
+    export GIT_SUBREPO_ROOT="$FORNIX_FOLDER/settings/extensions/git/subrepo/"
     export PATH="$GIT_SUBREPO_ROOT/lib:$PATH"
     export MANPATH="$GIT_SUBREPO_ROOT/man:$MANPATH"
     source "$GIT_SUBREPO_ROOT/share/enable-completion.sh"
