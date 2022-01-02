e8dcced18c4791732e05dd565085400445676c45
Jeff Hykin <jeff.hykin@gmail.com>
2022-01-02 16:05:54 -0500

    setup breakout no frame skip

--- main/agent_builders/a2c/baselines_recreation/a2c_experiment.py
+++ main/agent_builders/a2c/baselines_recreation/a2c_experiment.py
@@ -5,9 +5,11 @@ from stable_baselines3 import A2C
 # There already exists an environment generator
 # that will make and wrap atari environments correctly.
 # Here we are also multi-worker training (n_envs=4 => 4 environments)
-env = make_atari_env('PongNoFrameskip-v4', n_envs=4, seed=0)
+env = make_atari_env('BreakoutNoFrameskip-v4', n_envs=16, seed=0)
 # Frame-stacking with 4 frames
 env = VecFrameStack(env, n_stack=4)
+
+print('env.observation_space = ', env.observation_space)
 
 model = A2C('CnnPolicy', env, verbose=1)
 model.learn(total_timesteps=25_000)
