b57a09dd248ad287bea64c622fa3a3f8dd52f8c5
jeff.hykin <jeff.hykin@gmail.com>
2022-01-03 07:09:16 -0600

    setup optimized baselines a2c experiment

--- main/agent_builders/a2c/baselines_recreation/a2c_experiment.py
+++ main/agent_builders/a2c/baselines_recreation/a2c_experiment.py
@@ -1,21 +1,46 @@
 from stable_baselines3.common.env_util import make_atari_env
 from stable_baselines3.common.vec_env import VecFrameStack
 from main.agent_builders.a2c.baselines_recreation.hacked_a2c import A2C
+from main.agent_builders.a2c.baselines_optimizer import RMSpropTFLike
 
 from tools.debug import debug, ic
 
 # There already exists an environment generator
 # that will make and wrap atari environments correctly.
-# Here we are also multi-worker training (n_envs=4 => 4 environments)
-env = make_atari_env('BreakoutNoFrameskip-v4', n_envs=16, seed=0)
+# Here we are also multi-worker training (n_envs=16 => 16 environments)
+env = make_atari_env(
+    'BreakoutNoFrameskip-v4',
+    n_envs=16, # from atari optimized hyperparams
+    seed=0,
+)
 # Frame-stacking with 4 frames
-env = VecFrameStack(env, n_stack=4)
+env = VecFrameStack(
+    env,
+    n_stack=4, # from atari optimized hyperparams
+)
 
-print('env.observation_space = ', env.observation_space)
-
-model = A2C('CnnPolicy', env, verbose=1)
-model.learn(total_timesteps=25_000)
-
+# 
+# train
+# 
+model = A2C(
+    'CnnPolicy', # from atari optimized hyperparams
+    env,
+    verbose=1,
+    ent_coef=0.01, # from atari optimized hyperparams
+    vf_coef=0.25, # from atari optimized hyperparams
+    policy_kwargs=dict(
+        optimizer_class=RMSpropTFLike, # from atari optimized hyperparams
+        optimizer_kwargs=dict(eps=1e-5), # from atari optimized hyperparams
+    ),
+)
+model.learn(
+    total_timesteps=10_000_000, # from atari optimized hyperparams
+) 
+model.save("baselines_10_000_000_model.ignore.zip")
+  
+# 
+# test
+# 
 obs = env.reset()
 while True:
     action, _states = model.predict(obs)
