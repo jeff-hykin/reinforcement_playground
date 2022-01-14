afe2ebb633f9ee6ce310aaf0f2a4ff4bc4767f32
jeff.hykin <jeff.hykin@gmail.com>
2022-01-14 15:30:41 -0600

    setup hyperparams

--- main/prefabs/auto_imitator/traning.py
+++ main/prefabs/auto_imitator/traning.py
@@ -5,39 +5,74 @@ from tools.all_tools import *
 from tools.agent_recorder import AgentRecorder
 from prefabs.auto_imitator import AutoImitator
 
+from prefabs.helpful_fitness_measures import trend_up
+
 database = AgentRecorder(save_to="resources/datasets.ignore/atari/baselines_pretrained@vectorized_breakout")
-def learning_rate(timestep_index):
-    return 0.000197 - (timestep_index * 0.0001 * 0.00004)
 
-auto_imitator = AutoImitator(
-    learning_rate=learning_rate,
-    input_shape=(4,84,84),
-    latent_shape=(512,),
-    output_shape=(4,),
-    path="models.ignore/auto_imitator_offline_9.model",
-)
+# 
+# training
+# 
+training_number = 0
+def train(base_learning_rate):
+    training_number += 1
+    def learning_rate(timestep_index):
+        return base_learning_rate - (timestep_index * 0.0001 * 0.00004)
 
-logging = LazyDict(
-    smoothing_size=128,
-    should_log=Countdown(size=1000),
-    should_print=Countdown(size=100),
-    correctness_card=ss.DisplayCard("quickLine", []),
-    loss_card=ss.DisplayCard("quickLine", []),
-    update_correctness=lambda data: logging.correctness_card.send("clear").send(tuple(((index+1)*logging.smoothing_size, each) for index, each in enumerate(tuple(average(to_pure(each)) for each in bundle(data, bundle_size=logging.smoothing_size))))),
-    update_loss=lambda        data: logging.loss_card.send(       "clear").send(tuple(((index+1)*logging.smoothing_size, each) for index, each in enumerate(tuple(average(to_pure(each)) for each in bundle(data, bundle_size=logging.smoothing_size))))),
-)
-
-for index, (observations, actions) in enumerate(database.load_batch_data("64")):
-    if logging.should_print(): print(f'batch {index+1}/{database.batch_size}')
-    auto_imitator.update_weights(
-        batch_of_inputs=opencv_image_to_torch_image(observations),
-        batch_of_ideal_outputs=actions,
-        epoch_index=1,
-        batch_index=index
+    auto_imitator = AutoImitator(
+        learning_rate=learning_rate,
+        input_shape=(4,84,84),
+        latent_shape=(512,),
+        output_shape=(4,),
+        path=f"models.ignore/auto_imitator_hypertuning_{training_number}.model",
     )
+
+    logging = LazyDict(
+        smoothing_size=128,
+        should_log=Countdown(size=1000),
+        should_print=Countdown(size=100),
+        correctness_card=ss.DisplayCard("quickLine", []),
+        loss_card=ss.DisplayCard("quickLine", []),
+        update_correctness=lambda data: logging.correctness_card.send("clear").send(tuple(((index+1)*logging.smoothing_size, each) for index, each in enumerate(tuple(average(to_pure(each)) for each in bundle(data, bundle_size=logging.smoothing_size))))),
+        update_loss=lambda        data: logging.loss_card.send(       "clear").send(tuple(((index+1)*logging.smoothing_size, each) for index, each in enumerate(tuple(average(to_pure(each)) for each in bundle(data, bundle_size=logging.smoothing_size))))),
+    )
+
+    for index, (observations, actions) in enumerate(database.load_batch_data("64")):
+        if logging.should_print(): print(f'batch {index+1}/{database.batch_size}')
+        auto_imitator.update_weights(
+            batch_of_inputs=opencv_image_to_torch_image(observations),
+            batch_of_ideal_outputs=actions,
+            epoch_index=1,
+            batch_index=index
+        )
+        
+        if logging.should_log() or index == 0:
+            logging.update_correctness(auto_imitator.logging.proportion_correct_at_index)
+            logging.update_loss(auto_imitator.logging.loss_at_index)
+            auto_imitator.save()
     
-    if logging.should_log() or index == 0:
-        logging.update_correctness(auto_imitator.logging.proportion_correct_at_index)
-        logging.update_loss(auto_imitator.logging.loss_at_index)
-        auto_imitator.save()
-    
+    return auto_imitator
+
+
+# 
+# 
+# tuner
+# 
+#
+def tune_hyperparams(number_of_episodes_per_trial=100_000, fitness_func=trend_up):
+    import optuna
+    # connect the trial-object to hyperparams and setup a measurement of fitness
+    objective_func = lambda trial: fitness_func(
+        train(
+            base_learning_rate=trial.suggest_loguniform('learning_rate', 0.0005, 0.00001),
+        ).logging.proportion_correct_at_index
+    )
+    study = optuna.create_study(direction="maximize")
+    study.optimize(objective_func, n_trials=100)
+    return study
+
+# 
+# 
+# run
+# 
+# 
+study = tune_hyperparams
