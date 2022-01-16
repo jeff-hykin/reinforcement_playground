6a02a1ab0feb4b1fa88461f486a6ab324b857df7
jeff.hykin <jeff.hykin@gmail.com>
2022-01-16 10:14:49 -0600

    setup eval of baselines properly

--- main/missions/baselines_a2c@eval_breakout_vectorized.py
+++ main/missions/baselines_a2c@eval_breakout_vectorized.py
@@ -11,11 +11,11 @@ import tools.stat_tools as stat_tools
 from world_builders.atari.baselines_vectorized import Environment
 from agent_builders.baselines_a2c.main import Agent
 from prefabs.baselines_optimizer import RMSpropTFLike
-from prefabs.auto_imitator import AutoImitator
+from prefabs.helpful_fitness_measures import average
 
 env = Environment(
     name='BreakoutNoFrameskip-v4',
-    n_envs=4, # from atari optimized hyperparams
+    n_envs=1, # from atari optimized hyperparams
     seed=0,
     n_stack=4, # 4 frames, from atari optimized hyperparams
 )
@@ -37,58 +37,34 @@ mr_bond = Agent(
 # mr_bond.load("models.ignore/baselines_10_000_000.model")
 mr_bond.load("models.ignore/BreakoutNoFrameskip-v4.zip")
 
-#   
-# setup imitator  
-#   
-batch_index = -1
-next_group_triggered = Countdown(size=1000)
-auto_imitator = AutoImitator(
-    learning_rate=0.001,
-    input_shape=(4,84,84),
-    latent_shape=(512,),
-    output_shape=(4,),
-    path="models.ignore/auto_imitator_4.model",
-)
-
 # 
-# train and record
+# eval
 # 
-# loss_card = ss.DisplayCard("quickLine", [])
-# ss.DisplayCard("quickMarkdown", "#### Losses")
-# proportion_correct_card = ss.DisplayCard("quickLine", [])
-# ss.DisplayCard("quickMarkdown", "#### Proportion Correct")
+number_of_episodes = 100
 
-group_index = 0
-observations = env.reset()
+
+#   
+# mission
+#   
 mr_bond.when_mission_starts()
-mr_bond.when_episode_starts(0)
-reward_batches = []
-while True:
-    batch_index += 1
-    observations_in_torch_form = opencv_image_to_torch_image(observations)
+for episode_index in range(number_of_episodes):
     
-    mr_bond.observation = observations
-    mr_bond.when_timestep_starts(batch_index)
-    observations, rewards, dones, info = env.step(mr_bond.action)
-    reward_batches.append(rewards)
-    # auto_imitator.update_weights(
-    #     batch_of_inputs=observations_in_torch_form,
-    #     batch_of_ideal_outputs=mr_bond.action,
-    #     epoch_index=1,
-    #     batch_index=batch_index,
-    # )
-    mr_bond.when_timestep_ends(batch_index)
+    mr_bond.observation = env.reset()
+    mr_bond.reward = 0
+    mr_bond.episode_is_over = [False]
     
-    if batch_index < next_group_triggered.size:
-        print(f'checkin: batch_index: {batch_index}, average_reward: {to_tensor(reward_batches).mean()}')
+    mr_bond.when_episode_starts(episode_index)
+    timestep_index = -1
+    while not mr_bond.episode_is_over[0]:
+        timestep_index += 1
         
-    if next_group_triggered():
-        group_index += 1
-        average_correct = to_tensor(auto_imitator.logging.proportion_correct_at_index[-next_group_triggered.size:]).mean()
-        average_loss = to_tensor(auto_imitator.logging.loss_at_index[-next_group_triggered.size:]).mean()
-        print(f'batch_index: {batch_index}, average_correct: {average_correct}, average_loss: {average_loss}, average_reward: {to_tensor(reward_batches).mean()}')
-        # loss_card.send([group_index, average_loss])
-        # proportion_correct_card.send([group_index, average_correct])
-        auto_imitator.save()
-
-# ss.DisplayCard("quickLine", stat_tools.rolling_average(auto_imitator.logging.proportion_correct_at_index, 100))
+        mr_bond.when_timestep_starts(timestep_index)
+        mr_bond.observation, mr_bond.reward, mr_bond.episode_is_over, info = env.step(mr_bond.action)
+        mr_bond.when_timestep_ends(timestep_index)
+            
+    mr_bond.when_episode_ends(episode_index)
+    
+    print('average rewards = ', average(mr_bond.log.episode_rewards))
+    
+mr_bond.when_mission_ends()
+env.close()        
