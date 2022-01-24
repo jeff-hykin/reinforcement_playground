fixed the stupid softmax to sigmoid problem

main/prefabs/auto_imitator/main.py
@@ -1,4 +1,4 @@
-from super_map import LazyDict
+from super_map import LazyDict, Map
 
 from tools.all_tools import *
 
@@ -38,7 +38,7 @@ class AutoImitator(nn.Module):
         
         self.layers.add_module('encoder', self.encoder)
         self.layers.add_module('linear2'           , nn.Linear(in_features=latent_size, out_features=product(self.output_shape), bias=True),)
-        self.layers.add_module('linear2_activation', nn.Softmax(dim=0))
+        self.layers.add_module('linear2_activation', nn.Sigmoid())
         
         # optimizer
         self.optimizer = torch.optim.Adam(self.parameters(), lr=1)
@@ -49,35 +49,43 @@ class AutoImitator(nn.Module):
             optimizers=[ self.optimizer ],
         )
         
-        self.one_hotifier = OneHotifier(list(range(output_shape[0])))
-        # try to load from path if its given
-        if self.path:
-            try:
-                self.load()
-            except Exception as error:
-                pass
+        self.action_tensor_sum = torch.tensor([0,0,0,0]).float().to(self.hardware)
+        self.action_frequency = Map()
+        self.one_hotifier = OneHotifier(list(range(self.output_shape[0])))
+        # # try to load from path if its given
+        # if self.path:
+        #     try:
+        #         self.load()
+        #     except Exception as error:
+        #         pass
     
     @misc.all_args_to_tensor
     @misc.all_args_to_device
     def loss_function(self, model_output_batch, ideal_output_batch):
         which_ideal_actions = ideal_output_batch.long()
-        which_ideal_actions_one_hot = to_tensor([ self.one_hotifier.to_one_hot(each) for each in which_ideal_actions ])
-        print('model_output_batch.shape = ', model_output_batch.shape)
-        print('which_ideal_actions_one_hot = ', which_ideal_actions_one_hot)
+        which_ideal_actions_one_hot = to_tensor([ self.one_hotifier.to_one_hot(each) for each in which_ideal_actions ]).to(self.hardware)
+        which_model_actions = model_output_batch.argmax(dim=-1)
+        
+        number_correct_plus_one = 1 + (which_model_actions == which_ideal_actions).sum()/len(which_ideal_actions)
         # ideal output is vector of indicies, model_output_batch is vector of one-hot vectors
-        loss = torch.nn.functional.mse_loss(model_output_batch, which_ideal_actions_one_hot)
-        which_model_actions = model_output_batch.detach().argmax(dim=-1)
-        self.logging.proportion_correct_at_index.append( (which_model_actions == which_ideal_actions).sum()/len(which_ideal_actions) )
+        loss = torch.nn.functional.mse_loss(model_output_batch*100, which_ideal_actions_one_hot*100)
+        # loss = 10000 * (1/torch.std(model_output_batch, dim=1).sum())
+        which_model_actions = which_model_actions.detach()
+        for each in model_output_batch:
+            self.action_tensor_sum += each
+        for each in which_model_actions:
+            self.action_frequency[to_pure(each)] += 1
+        self.logging.proportion_correct_at_index.append( to_pure(number_correct_plus_one-1) )
         self.logging.loss_at_index.append(to_pure(loss))
         return loss
     
     @misc.all_args_to_tensor
     @misc.all_args_to_device
-    def update_weights(self, batch_of_inputs, batch_of_ideal_output_batchs, epoch_index, batch_index):
+    def update_weights(self, batch_of_inputs, batch_of_ideal_outputs, epoch_index, batch_index):
         self.learning_rate_scheduler.when_weight_update_starts()
         self.optimizer.zero_grad()
         batch_of_actual_outputs = self.forward(batch_of_inputs)
-        loss = self.loss_function(batch_of_actual_outputs, batch_of_ideal_output_batchs)
+        loss = self.loss_function(batch_of_actual_outputs, batch_of_ideal_outputs)
         loss.backward()
         self.optimizer.step()
         return loss