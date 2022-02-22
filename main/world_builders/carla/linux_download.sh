a98a5363ec2e00a67da9ecc26dda8976e2b0cad1
Jeff Hykin <jeff.hykin@gmail.com>
2022-02-22 11:07:07 -0600

    fix the download script

--- main/world_builders/carla/linux_download.sh
+++ main/world_builders/carla/linux_download.sh
@@ -1,5 +1,7 @@
 this_folder="$FORNIX_FOLDER/main/world_builders/carla"
 
+mkdir -p "$this_folder/workspace.ignore/"
+
 carla_download_path="$this_folder/workspace.ignore/carla_download.tar"
 curl https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/CARLA_0.9.13.tar.gz --output "$carla_download_path"
 {
