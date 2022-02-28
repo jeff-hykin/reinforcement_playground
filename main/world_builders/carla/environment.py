3e45111f397f54d51e67ea0215e4b6c61466e7e9
Jeff Hykin <jeff.hykin@gmail.com>
2022-02-28 13:14:04 -0600

    start setting up gym env for carla

--- main/world_builders/carla/environment.py
+++ main/world_builders/carla/environment.py
@@ -1,31 +1,20 @@
-from main.world_builders.carla.tools import Carla, carla
+import carla_env
 
-def main():
-    carla_instance = Carla(
-        settings=dict(
-            synchronous_mode=True,
-            fixed_delta_seconds=1.0/10,
-        ),
-    )
+carla_env.register(
+    id='CarlaEnv-state-town01-v1',
+    entry_point='carla_env.carla_env:CarlaEnv',
+    max_episode_steps=500,
+    kwargs={
+        'render': True,
+        'carla_port': 2000,
+        'changing_weather_speed': 0.1,
+        'frame_skip': 1,
+        'observations_type': 'state',
+        'traffic': True,
+        'vehicle_name': 'tesla.cybertruck',
+        'map_name': 'Town01',
+        'autopilot': True
+    }
+)
 
-    # Spawn the vehicle.
-    vehicle = carla_instance.spawn_vehicle()
-    # location = vehicle.get_location()
-    # location.x += 40
-    # vehicle.set_location(location)
 
-    # Spawn the camera and register a function to listen to the images.
-    camera = carla_instance.spawn_camera(
-        location=carla.Location(x=2.0, y=0.0, z=1.8),
-        rotation=carla.Rotation(roll=0, pitch=0, yaw=0),
-        attach_to=vehicle,
-    )
-    camera.listen(
-        lambda image:
-            image.save_to_disk(
-                f'carla.output.ignore/{image.frame:06d}.png',
-                carla.ColorConverter.LogarithmicDepth,
-            )
-    )
-    
-    carla_instance.world.tick()
--- main/world_builders/carla/tools.py
+++ main/world_builders/carla/tools.py
@@ -3,6 +3,39 @@ import time
 import carla
 
 class Carla:
+    """
+        from main.world_builders.carla.tools import Carla, carla
+
+        def main():
+            carla_instance = Carla(
+                settings=dict(
+                    synchronous_mode=True,
+                    fixed_delta_seconds=1.0/10,
+                ),
+            )
+
+            # Spawn the vehicle.
+            vehicle = carla_instance.spawn_vehicle()
+            # location = vehicle.get_location()
+            # location.x += 40
+            # vehicle.set_location(location)
+
+            # Spawn the camera and register a function to listen to the images.
+            camera = carla_instance.spawn_camera(
+                location=carla.Location(x=2.0, y=0.0, z=1.8),
+                rotation=carla.Rotation(roll=0, pitch=0, yaw=0),
+                attach_to=vehicle,
+            )
+            camera.listen(
+                lambda image:
+                    image.save_to_disk(
+                        f'carla.output.ignore/{image.frame:06d}.png',
+                        carla.ColorConverter.LogarithmicDepth,
+                    )
+            )
+            
+            carla_instance.world.tick()
+    """
     def __init__(self, address='localhost', port=2000, settings=dict()):
         self.client = carla.Client(address, port)
         self.world = client.get_world()
