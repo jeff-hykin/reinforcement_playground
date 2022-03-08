1e1b5452c996f416ad463b62854959a3383aa63e
Jeff Hykin <jeff.hykin@gmail.com>
2022-03-08 15:13:36 -0600

    clean up nix

--- settings/extensions/nix/shell.nix
+++ settings/extensions/nix/shell.nix
@@ -19,13 +19,104 @@
             ("__FORNIX_NIX_MAIN_CODE_PATH")
         )
     ),
-    nixgl ? (builtins.import
-        (fetchTarball "https://github.com/guibou/nixGL/archive/17658df1e17a64bc23ee5c93cfa9e8b663a4ac81.tar.gz")
-        {}
-    )
 }:
     # Lets setup some definitions
     let        
+        # 
+        # sources
+        # 
+        nixgl = (builtins.import
+            (fetchTarball "https://github.com/guibou/nixGL/archive/17658df1e17a64bc23ee5c93cfa9e8b663a4ac81.tar.gz")
+            {}
+        );
+        pkgsWithTorch_1_8_1 = (builtins.import
+            (builtins.fetchTarball 
+                ({
+                    url = "https://github.com/NixOS/nixpkgs/archive/141439f6f11537ee349a58aaf97a5a5fc072365c.tar.gz";
+                })
+            )
+            ({})
+        );
+        pkgsWithTorch_1_9_0 = (builtins.import
+            (builtins.fetchTarball 
+                ({
+                    url = "https://github.com/NixOS/nixpkgs/archive/c82b46413401efa740a0b994f52e9903a4f6dcd5.tar.gz";
+                })
+            )
+            ({})
+        );
+        pkgsWithNcclCudaToolkit_11_2 =  (builtins.import
+            (builtins.fetchTarball 
+                ({
+                    url = "https://github.com/NixOS/nixpkgs/archive/2cdd608fab0af07647da29634627a42852a8c97f.tar.gz";
+                })
+            )
+            ({})
+        );
+        pkgsWithWorkingShap =  (builtins.import
+            (builtins.fetchTarball 
+                ({
+                    url = "https://github.com/NixOS/nixpkgs/archive/3f50332bc4913a56ad216ca01f5d0bd24277a6b2.tar.gz";
+                })
+            )
+            ({})
+        );
+        
+        # 
+        # 
+        # packages
+        # 
+        # 
+        python = pkgsWithTorch_1_9_0.python38;
+        pythonPackages = pkgsWithTorch_1_9_0.python38Packages;
+        selectedCommonPythonPackages = [
+            pythonPackages.black
+            pythonPackages.poetry
+            pythonPackages.setuptools
+            pythonPackages.pyopengl
+            pythonPackages.pip
+            pythonPackages.virtualenv
+            pythonPackages.wheel
+            pythonPackages.numpy
+            pkgsWithWorkingShap.python38Packages.shap
+        ];
+        
+        # 
+        # for pytorch
+        # 
+        cudaStuff = (
+            let 
+                cudatoolkit = pkgsWithTorch_1_9_0.cudaPackages.cudatoolkit_11_2;
+                cudnn = pkgsWithTorch_1_9_0.cudnn_cudatoolkit_11_2;
+                nccl = pkgsWithNcclCudaToolkit_11_2.nccl_cudatoolkit_11;
+                magma = (pkgsWithTorch_1_9_0.magma.override
+                    ({
+                        cudatoolkit = cudatoolkit;
+                    })
+                );
+                pytorchWithCuda = (pythonPackages.pytorchWithCuda.override 
+                    ({
+                        cudaSupport = true;
+                        cudatoolkit = cudatoolkit;
+                        cudnn = cudnn;
+                        nccl = nccl;
+                        magma = magma;
+                    })
+                );
+                nvidia_x11 = pkgsWithTorch_1_9_0.linuxPackages.nvidia_x11;
+            in
+                # return all this stuff
+                {
+                    cudatoolkit = cudatoolkit;
+                    cudnn = cudnn;
+                    magma = magma;
+                    nccl = nccl;
+                    pytorchWithCuda = pytorchWithCuda;
+                    nvidia_x11 = nvidia_x11;
+                    pythonPackages = pkgsWithTorch_1_9_0.python38Packages;
+                }
+        );
+        
         # just a helper
         emptyOptions = ({
             buildInputs = [];
@@ -33,78 +124,29 @@
             shellCode = "";
         });
         
-        salt = (builtins.import
-            (./nixpkgs/salt.nix)
-            main
-        );
-        
-        # torch = (builtins.import
-        #     (fetchTarball "https://github.com/jeff-hykin/pytorch_nixpkg/archive/77961dce25528445a7e4e448652754079deb6f73.tar.gz")
-        #     {
-        #         pkgs = main.packages // {
-        #             cudnn_cudatoolkit_11_2 = main.packages.cudnn_cudatoolkit_11_2;
-        #         };
-        #     }
-        # );
-        magma = (main.packages.magma.override
-            ({
-                cudatoolkit = main.packages.cudaPackages.cudatoolkit_11_2;
-            })
-        );
-        nccl_cudatoolkit_11_2 = (builtins.import
-            (builtins.fetchTarball 
-                ({
-                    url = "https://github.com/NixOS/nixpkgs/archive/2cdd608fab0af07647da29634627a42852a8c97f.tar.gz";
-                })
-            )
-            ({})
-        ).nccl_cudatoolkit_11;
-           
-        pytorchWithCudaUnpatched = (builtins.import
-            # 
-            # older version with pytorch 1.8.1: 141439f6f11537ee349a58aaf97a5a5fc072365c
-            # 
-            (builtins.fetchTarball 
-                ({
-                    url = "https://github.com/NixOS/nixpkgs/archive/c82b46413401efa740a0b994f52e9903a4f6dcd5.tar.gz";
-                })
-            )
-            ({})
-        ).python38Packages.pytorchWithCuda;
-        pytorchWithCuda = (pytorchWithCudaUnpatched.override 
-            ({
-                cudaSupport = true;
-                cudatoolkit = main.packages.cudaPackages.cudatoolkit_11_2;
-                cudnn = main.packages.cudnn_cudatoolkit_11_2;
-                nccl = nccl_cudatoolkit_11_2;
-                magma = magma;
-            })
-        );
-
-        
         # 
         # Linux Only
         #
         linuxOnly = if main.stdenv.isLinux then ({
-            buildInputs = [
+            buildInputs = selectedCommonPythonPackages ++ [
+                pythonPackages.pybullet
                 nixgl.auto.nixGLNvidia
-                main.packages.cudaPackages.cudatoolkit_11_2
-                main.packages.cudnn_cudatoolkit_11_2
-                pytorchWithCuda
-                # (torch { name = "torch";})
+                cudaStuff.cudatoolkit
+                cudaStuff.cudnn
+                cudaStuff.pytorchWithCuda
             ];
             nativeBuildInputs = [];
             shellCode = ''
                 if [[ "$OSTYPE" == "linux-gnu" ]] 
                 then
                     true # add important (LD_LIBRARY_PATH, PATH, etc) nix-Linux code here
-                    export CUDA_PATH="${main.packages.cudaPackages.cudatoolkit_11_2}"
-                    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${main.packages.linuxPackages.nvidia_x11}/lib:${main.packages.ncurses5}/lib:/run/opengl-driver/lib"
-                    export EXTRA_LDFLAGS="$EXTRA_CCFLAGS:-L/lib -L${main.packages.linuxPackages.nvidia_x11}/lib"
+                    export CUDA_PATH="${cudaStuff.cudatoolkit}"
+                    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${cudaStuff.nvidia_x11}/lib"
+                    export EXTRA_LDFLAGS="$EXTRA_CCFLAGS:-L/lib -L${cudaStuff.nvidia_x11}/lib"
                     export LD_LIBRARY_PATH="$(${nixgl.auto.nixGLNvidia}/bin/nixGLNvidia-470.86 printenv LD_LIBRARY_PATH):$LD_LIBRARY_PATH"
                     export EXTRA_CCFLAGS="$EXTRA_CCFLAGS:-I/usr/include"
                     export LD_LIBRARY_PATH="${main.makeLibraryPath [ main.packages.glib ] }:$LD_LIBRARY_PATH"
-                    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${main.packages.ncurses}/lib:/run/opengl-driver/lib"
+                    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/run/opengl-driver/lib"
                     
                     export LD_LIBRARY_PATH="${main.packages.hdf5}:$LD_LIBRARY_PATH"
                     export LD_LIBRARY_PATH="${main.packages.openmpi}/lib:$LD_LIBRARY_PATH"
@@ -113,16 +155,16 @@
                     export LD_LIBRARY_PATH="${main.packages.zlib}/lib:$LD_LIBRARY_PATH"
 
                     # CUDA and magma path
-                    export LD_LIBRARY_PATH="${main.packages.cudaPackages.cudatoolkit_11_2}/lib:${main.packages.cudnn_cudatoolkit_11_2}/lib:${magma}/lib:$LD_LIBRARY_PATH"
+                    export LD_LIBRARY_PATH="${cudaStuff.cudatoolkit}/lib:${cudaStuff.cudnn}/lib:${cudaStuff.magma}/lib:$LD_LIBRARY_PATH"
                 fi
             '';
             # for python with CUDA 
             # 1. install cuda drivers on the main machine then
             # 2. include the following inside the shellCode if statement above
             #     export CUDA_PATH="${main.packages.cudatoolkit}"
-            #     export EXTRA_LDFLAGS="-L/lib -L${main.packages.linuxPackages.nvidia_x11}/lib"
+            #     export EXTRA_LDFLAGS="-L/lib -L${cudaStuff.nvidia_x11}/lib"
             #     export EXTRA_CCFLAGS="-I/usr/include"
-            #     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${main.packages.linuxPackages.nvidia_x11}/lib:${main.packages.ncurses5}/lib:/run/opengl-driver/lib"
+            #     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${cudaStuff.nvidia_x11}/lib:${main.packages.ncurses5}/lib:/run/opengl-driver/lib"
             #     export LD_LIBRARY_PATH="$(${main.packages.nixGLNvidia}/bin/nixGLNvidia printenv LD_LIBRARY_PATH):$LD_LIBRARY_PATH"
             #     export LD_LIBRARY_PATH="${main.makeLibraryPath [ main.packages.glib ] }:$LD_LIBRARY_PATH"
             # 3. then add the following to the nix.toml file
@@ -182,7 +224,9 @@
         # Mac Only
         # 
         macOnly = if main.stdenv.isDarwin then ({
-            buildInputs = [];
+            buildInputs = selectedCommonPythonPackages ++ [
+                pythonPackages.torch
+            ];
             nativeBuildInputs = [];
             shellCode = ''
                 if [[ "$OSTYPE" = "darwin"* ]] 
@@ -200,7 +244,7 @@
         # 
         main.packages.mkShell {
             # inside that shell, make sure to use these packages
-            buildInputs =  main.project.buildInputs ++ macOnly.buildInputs ++ linuxOnly.buildInputs ++ [ salt ];
+            buildInputs =  main.project.buildInputs ++ macOnly.buildInputs ++ linuxOnly.buildInputs;
             
             nativeBuildInputs =  main.project.nativeBuildInputs ++ macOnly.nativeBuildInputs ++ linuxOnly.nativeBuildInputs;
             
@@ -214,7 +258,7 @@
                 ${macOnly.shellCode}
                 
                 # provide access to ncurses for nice terminal interactions
-                export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${main.packages.ncurses}/lib"
+                export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${main.packages.ncurses5}/lib"
                 export LD_LIBRARY_PATH="${main.makeLibraryPath [ main.packages.glib ] }:$LD_LIBRARY_PATH"
                 
                 if [ "$FORNIX_DEBUG" = "true" ]; then
