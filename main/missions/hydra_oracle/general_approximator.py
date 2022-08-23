c20ccd09482e024cb2ce644f19194d4b89310869
Jeff Hykin <jeff.hykin@gmail.com>
2022-08-23 12:33:45 -0500

    finally create a general approximator that works

--- main/missions/hydra_oracle/general_approximator.py
+++ main/missions/hydra_oracle/general_approximator.py
@@ -2,12 +2,12 @@ import math
 
 import torch
 import torch.nn as nn
-from trivial_torch_tools import Sequential, init, convert_each_arg
-from trivial_torch_tools.generics import product
+from trivial_torch_tools import Sequential, init, convert_each_arg, to_tensor
+from trivial_torch_tools.generics import product, to_pure
 from super_map import LazyDict
 
 mse_loss = nn.MSELoss()
-class GeneralApproximator(nn.Module):
+class GeneralApproximator(nn.Module): # NOTE: this thing is broken! it should work but for some reason fails to learn abstractions
     @init.to_device()
     @init.forward_sequential_method
     def __init__(self, input_shape, output_shape, outputs_are_between_0_and_1=False, hyperparams=None):
@@ -20,7 +20,7 @@ class GeneralApproximator(nn.Module):
         output_size = product(self.output_shape)
         
         big_size = max(input_size, output_size)
-        number_of_middle_layers = math.floor(math.log(math.sqrt(big_size))/math.log(4)) or 1 # log base 4 was hand picked with no data basis, 
+        number_of_middle_layers = (math.floor(math.log(math.sqrt(big_size))/math.log(4)) or 1) + 1 # log base 4 was hand picked with no data basis, 
             # big_size=10 => layers=0, 
             # big_size=1_000 => layers=2, 
             # big_size=50_000 => layers=3, 
@@ -60,7 +60,6 @@ class GeneralApproximator(nn.Module):
     def fit(self, inputs, correct_outputs, epochs=1):
         for _ in range(epochs):
             self.optimizer.zero_grad()
-            inputs.requires_grad = True
             current_output = self.forward(inputs)
             loss = self.hyperparams.loss_function(current_output, correct_outputs)
             loss.backward()
@@ -73,46 +72,74 @@ class GeneralApproximator(nn.Module):
         with torch.no_grad():
             return self.forward(inputs)
 
+from sklearn.neighbors import NearestNeighbors
+import numpy
+class GeneralApproximator:
+    def __init__(self, input_shape, output_shape, hyperparams=None):
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.inputs = []
+        self.outputs = []
+        self.hyperparams = LazyDict(
+            n_neighbors=3,
+            algorithm='ball_tree',
+        ).update(hyperparams or {})
+        self.model = NearestNeighbors(**self.hyperparams)
+    
+    def preprocess(self, inputs):
+        # flatten each input, 1 row = 1 input
+        return torch.flatten(to_tensor(inputs), 1).numpy()
+        
+    def fit(self, inputs, correct_outputs):
+        self.inputs += inputs
+        self.outputs += correct_outputs
+        self.model.fit(self.preprocess(inputs))
+    
+    def predict(self, inputs):
+        # if no data has been gathered, just return random outputs
+        # TODO: this should maybe show a warning
+        if len(self.inputs) == 0:
+            return torch.rand((len(inputs), self.output_shape))
+        distances_for_inputs, indices_for_inputs = self.model.kneighbors( self.preprocess(inputs) )
+        outputs = []
+        for each_input, each_distances, each_indices in zip(inputs, distances_for_inputs, indices_for_inputs):
+            distances = to_pure(each_distances)
+            values    = tuple( to_pure(self.outputs[each_index]) for each_index in each_indices )
+            values_as_array = to_tensor(values).numpy()
+            average_value = numpy.mean(values_as_array, axis=0)
+            outputs.append(average_value)
+            # TODO: normalize and weight by distance
+        return to_pure(outputs)
 
-def generate_test_data_1(number_of_values=1000):
-    inputs = []
-    outputs = []
-    import random
-    for each in range(number_of_values):
-        if each % 2:
-            inputs.append(
-                torch.tensor([
-                    300 + random.random()*3,
-                    300 + random.random()*3,
-                    300 + random.random()*3,
-                    300 + random.random()*3,
-                ]),
-            )
-            outputs.append(
-                torch.tensor([
-                    1.0 + random.random(),
-                ])
-            )
-        else:
-            inputs.append(
-                torch.tensor([
-                    100 + random.random()*3,
-                    100 + random.random()*3,
-                    100 + random.random()*3,
-                    100 + random.random()*3,
-                ]),
-            )
-            outputs.append(
-                torch.tensor([
-                    -1.0 - random.random(),
-                ])
-            )
-    return inputs, outputs
 
+def test_the_approximator():
+    input_shape = (2, 4)
+    def generate_test_data_1(number_of_values=1000):
+        inputs = []
+        outputs = []
+        import random
+        for each in range(number_of_values):
+            if each % 2:
+                the_input = torch.ones(input_shape) + random.random()
+                print(f'''the_input.shape = {the_input.shape}''')
+                inputs.append(
+                    the_input,
+                )
+                outputs.append(
+                    to_tensor([ 1000 , -2]) * random.random(),
+                )
+            else:
+                inputs.append(
+                    torch.zeros(input_shape),
+                )
+                outputs.append(
+                    to_tensor([ -1000, 2 ]) * random.random(),
+                )
+        return inputs, outputs
 
-
-inputs, correct_outputs = generate_test_data_1(10000000)
-
-approximator = GeneralApproximator(input_shape=[4], output_shape=[1], hyperparams=LazyDict(layer_sizes=[10,3]))
-approximator.fit(inputs=inputs, correct_outputs=correct_outputs)
-approximator.predict(inputs=[[300,300,300,300]])
+    inputs, correct_outputs = generate_test_data_1(100)
+    # inputs, correct_outputs = generate_test_data_1(1000000)
+    approximator = GeneralApproximator(input_shape=input_shape, output_shape=[1])
+    approximator.fit(inputs=inputs, correct_outputs=correct_outputs)
+    approximator.predict(inputs=inputs[0:4]) # approximator.predict(inputs=[[300,300,300,300]])
+    correct_outputs[0:4]
