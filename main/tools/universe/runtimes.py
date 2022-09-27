416cbeb053ac73b4c3a5a14b11b1f896b9dcfc32
Jeff Hykin <jeff.hykin@gmail.com>
2022-09-27 12:36:36 -0500

    make basic runtime more useful

--- main/tools/universe/runtimes.py
+++ main/tools/universe/runtimes.py
@@ -4,11 +4,15 @@ from copy import deepcopy
 from tools.universe.timestep import Timestep
 
 
-def basic(*, agent, env, max_timestep_index=math.inf, max_episode_index=math.inf):
+def basic(*, agent, env, max_timestep_index=math.inf, max_episode_index=math.inf, observation_modifier=None, reaction_modifier=None, reward_modifier=None):
     """
     for episode_index, timestep_index, agent.timestep.observation, agent.timestep.reward, agent.timestep.is_last_step in traditional_runtime(agent=agent, env=env):
         pass
     """
+    if observation_modifier is None: observation_modifier = lambda each: each
+    if reaction_modifier    is None: reaction_modifier    = lambda each: each
+    if reward_modifier      is None: reward_modifier      = lambda each: each
+    
     agent.when_mission_starts()
     
     for episode_index in itertools.count(0): # starting at 0
@@ -21,7 +25,7 @@ def basic(*, agent, env, max_timestep_index=math.inf, max_episode_index=math.inf
         )
         agent.next_timestep = Timestep(
             index=0,
-            observation=deepcopy(env.reset()),
+            observation=observation_modifier(deepcopy(env.reset())),
             is_last_step=False,
         )
         agent.when_episode_starts()
@@ -32,11 +36,13 @@ def basic(*, agent, env, max_timestep_index=math.inf, max_episode_index=math.inf
             agent.next_timestep     = Timestep(index=agent.next_timestep.index+1)
             
             agent.when_timestep_starts()
-            if type(agent.timestep.reaction) == type(None):
-                agent.timestep.reaction = env.action_space.sample()
-            observation, reward, is_last_step, agent.timestep.hidden_info = env.step(agent.timestep.reaction)
-            agent.next_timestep.observation = deepcopy(observation)
-            agent.timestep.reward           = deepcopy(reward)
+            reaction = agent.timestep.reaction
+            if type(reaction) == type(None):
+                reaction = env.action_space.sample()
+            reaction = reaction_modifier(reaction)
+            observation, reward, is_last_step, agent.timestep.hidden_info = env.step(reaction)
+            agent.next_timestep.observation = observation_modifier(deepcopy(observation))
+            agent.timestep.reward           = reward_modifier(deepcopy(reward))
             agent.timestep.is_last_step     = deepcopy(is_last_step)
             agent.when_timestep_ends()
             
